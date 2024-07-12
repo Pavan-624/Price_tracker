36f57330d62c1af00ddde800b8ef30566c7bcaf1
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-12 18:09:22 +0530

    chnaged yml file to run for every 30 min and also code and secrets

--- price_tracker.py
+++ price_tracker.py
@@ -46,9 +46,9 @@ def fetch_data():
         chrome_options = Options()
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Add this to avoid detection
-        chrome_options.add_argument('--headless')  # Run in headless mode
-        chrome_options.add_argument('--no-sandbox')  # Disable sandboxing
-        chrome_options.add_argument('--disable-dev-shm-usage')  # Disable /dev/shm usage
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
         # Initialize the Chrome WebDriver using ChromeDriverManager
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
