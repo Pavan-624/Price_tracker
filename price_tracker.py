a8012bdb1ea273a704ca94d8c0d5b7fb6267877b
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-20 18:48:41 +0530

    Initial commit

--- price_tracker.py
+++ price_tracker.py
@@ -1,23 +1,30 @@
+import os
+import json
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import time
-import os
 
-# Fetch environment variables
-from_email = os.getenv('FROM_EMAIL')
-from_password = os.getenv('EMAIL_PASSWORD')
-to_email = os.getenv('TO_EMAIL')
+# Load environment variables
+env_data = os.getenv('ENV_DATA')
 
-# SMTP server configuration
+if env_data:
+    env_vars = json.loads(env_data)
+    print("Loaded environment variables")
+else:
+    raise ValueError("ENV_DATA is not set or empty")
+
+from_email = env_vars.get('FROM_EMAIL', '')
+from_password = env_vars.get('EMAIL_PASSWORD', '')
+to_email = env_vars.get('TO_EMAIL', '')
+
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
 
-# URL and threshold for a single product
 product = {
     "url": "https://www.amazon.in/Fossil-Analog-Black-Unisex-Watch/dp/B005LBZ6G6",
     "threshold": 141489.0
@@ -42,29 +49,24 @@ def send_email(subject, body, to_email):
 def fetch_data():
     driver = None
     try:
-        chrome_options = Options()
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Add this to avoid detection
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
 
-        # Specify the path to the Chrome binary and ChromeDriver executable
-        # Download ChromeDriver from: https://chromedriver.chromium.org/downloads
-        # Download Google Chrome from: https://www.google.com/chrome/
-        chrome_options.binary_location = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
-        driver = webdriver.Chrome(service=Service(r'C:\Users\Pavan\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe'), options=chrome_options)
+        # Define path for GeckoDriver
+        geckodriver_path = '/snap/bin/geckodriver'
+        service = Service(geckodriver_path)
 
-        delay_time = 10  # Fixed delay time in seconds
+        # Initialize the Firefox WebDriver
+        driver = webdriver.Firefox(service=service, options=firefox_options)
+        
+        delay_time = 10
 
-        # Process a single product
         driver.get(product["url"])
-        time.sleep(delay_time)  # Use fixed delay time
+        time.sleep(delay_time)
 
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
 
-        # Adjust these selectors based on actual HTML structure
         name_tag = soup.find('span', class_='a-size-large product-title-word-break')
         name = name_tag.text.strip() if name_tag else 'N/A'
         print(f"Product Name: {name}")
@@ -77,7 +79,6 @@ def fetch_data():
             price = float(price)
             print(f"Fetched Price: {price}")
 
-            # Always send an email if the price is below the threshold
             if price <= product["threshold"]:
                 print(f'Price is below threshold for {name}: {price}')
                 send_email(
