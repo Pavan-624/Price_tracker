1b862b94f7b24c9c2bb26d18a8d5da040437e015
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-20 21:24:18 +0530

    using css selector total code

--- price_tracker.py
+++ price_tracker.py
@@ -1,6 +1,53 @@
+import os
+import json
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
+
+# Load environment variables
+env_data = os.getenv('ENV_DATA')
+
+if env_data:
+    env_vars = json.loads(env_data)
+    print("Loaded environment variables")
+else:
+    raise ValueError("ENV_DATA is not set or empty")
+
+from_email = env_vars.get('FROM_EMAIL', '')
+from_password = env_vars.get('EMAIL_PASSWORD', '')
+to_email = env_vars.get('TO_EMAIL', '')
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+product = {
+    "url": "https://www.amazon.in/Fossil-Analog-Black-Unisex-Watch/dp/B005LBZ6G6",
+    "threshold": 141489.0
+}
+
+def send_email(subject, body, to_email):
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(from_email, from_password)
+            server.send_message(msg)
+            print(f"Email sent to {to_email}")
+    except Exception as e:
+        print(f"An error occurred while sending email: {e}")
 
 def fetch_data():
     driver = None
@@ -29,6 +76,30 @@ def fetch_data():
         # Example: Print text of each price element
         for element in price_elements:
             print(element.text)
+
+        # Fetch the product name
+        name_tag = driver.find_element(By.CSS_SELECTOR, '.a-size-large.product-title-word-break')
+        name = name_tag.text.strip() if name_tag else 'N/A'
+        print(f"Product Name: {name}")
+
+        # Fetch the product price
+        price_tag = driver.find_element(By.CSS_SELECTOR, '.a-price-whole')
+        price = price_tag.text.strip().replace(',', '') if price_tag else 'N/A'
+        print(f"Product Price: {price}")
+
+        if price != 'N/A':
+            price = float(price)
+            print(f"Fetched Price: {price}")
+
+            if price <= product["threshold"]:
+                print(f'Price is below threshold for {name}: {price}')
+                send_email(
+                    'Price Drop Alert!',
+                    f'The price of {name} has dropped to {price}.',
+                    to_email
+                )
+        else:
+            print("Price data not available.")
 
     except Exception as e:
         print(f"An error occurred: {e}")
